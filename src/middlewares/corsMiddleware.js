6b275ab88b87a59dccc4e3533dca8959291cf41e
Ehsan Gazar <me@ehsangazar.com>
2023-04-28 09:19:33 +1000

    fix cors middleware

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -1,11 +1,10 @@
-const corsMiddleware = (req, res, next) => {
+const corsMiddleware = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, HEAD, OPTIONS, POST, PUT, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "*");
-  next();
 };
 
 export default corsMiddleware;
