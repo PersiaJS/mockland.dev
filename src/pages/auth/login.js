c25eeb67aea043805b1e979bc2ee0a3a921b458d
Maral Najafi <marawlnajafi@gmail.com>
2023-05-06 15:58:53 +0330

    commented out prev login form and imported new login from

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -18,6 +18,7 @@ import fetchHandler from "../../utils/fetchHandler";
 import { useState } from "react";
 import Link from "next/link";
 import Cookies from "universal-cookie";
+import LoginForm from "@/components/LoginForm/LoginForm";
 
 const SignupSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Required"),
@@ -28,111 +29,112 @@ const Login = () => {
   const [message, setMessage] = useState(null);
 
   return (
-    <Container>
-      <Box display={"grid"} alignItems={"center"} height={"100vh"}>
-        <Box>
-          <Box textAlign={"center"}>
-            <Heading as={"h1"}>Login</Heading>
-          </Box>
-          <Box p={4}>
-            <Formik
-              initialValues={{ name: "Sasuke" }}
-              validationSchema={SignupSchema}
-              onSubmit={async (values, actions) => {
-                try {
-                  const response = await fetchHandler.post(
-                    "/api/member/login",
-                    values
-                  );
-                  if (response.data.status) {
-                    const cookies = new Cookies();
-                    cookies.set("auth", response.data.auth);
-                    setMessage({
-                      status: "success",
-                      message: response.data.message,
-                    });
-                  } else {
-                    setMessage({
-                      status: "warning",
-                      message: response.data.message,
-                    });
-                  }
-                } catch (error) {
-                  setMessage({
-                    status: "error",
-                    message: "Something went wrong. Please try again later.",
-                  });
-                }
-              }}
-            >
-              {(props) => (
-                <Form>
-                  <Field name="email">
-                    {({ field, form }) => (
-                      <FormControl
-                        isInvalid={form.errors.email && form.touched.email}
-                      >
-                        <FormLabel>Email</FormLabel>
-                        <Input type="email" {...field} placeholder="Email" />
-                        <FormErrorMessage>{form.errors.email}</FormErrorMessage>
-                      </FormControl>
-                    )}
-                  </Field>
-                  <Field name="password">
-                    {({ field, form }) => (
-                      <FormControl
-                        isInvalid={
-                          form.errors.password && form.touched.password
-                        }
-                      >
-                        <FormLabel>Password</FormLabel>
-                        <Input
-                          type="password"
-                          {...field}
-                          placeholder="Password"
-                        />
-                        <FormErrorMessage>
-                          {form.errors.password}
-                        </FormErrorMessage>
-                      </FormControl>
-                    )}
-                  </Field>
-                  <Button
-                    mt={4}
-                    colorScheme="teal"
-                    isLoading={props.isSubmitting}
-                    type="submit"
-                  >
-                    Login
-                  </Button>
-                </Form>
-              )}
-            </Formik>
-            {message && (
-              <Box py={4}>
-                <Alert status={message.status}>
-                  <AlertIcon />
-                  {message.message}
-                </Alert>
-              </Box>
-            )}
-            <Box mt={8}>
-              <UnorderedList>
-                <ListItem>
-                  <Link href="/">Homepage</Link>
-                </ListItem>
-                <ListItem>
-                  <Link href="/auth/register">Register</Link>
-                </ListItem>
-                <ListItem>
-                  <Link href="/auth/forget">Forget Password</Link>
-                </ListItem>
-              </UnorderedList>
-            </Box>
-          </Box>
-        </Box>
-      </Box>
-    </Container>
+    // <Container>
+    //   <Box display={"grid"} alignItems={"center"} height={"100vh"}>
+    //     <Box>
+    //       <Box textAlign={"center"}>
+    //         <Heading as={"h1"}>Login</Heading>
+    //       </Box>
+    //       <Box p={4}>
+    //         <Formik
+    //           initialValues={{ name: "Sasuke" }}
+    //           validationSchema={SignupSchema}
+    //           onSubmit={async (values, actions) => {
+    //             try {
+    //               const response = await fetchHandler.post(
+    //                 "/api/member/login",
+    //                 values
+    //               );
+    //               if (response.data.status) {
+    //                 const cookies = new Cookies();
+    //                 cookies.set("auth", response.data.auth);
+    //                 setMessage({
+    //                   status: "success",
+    //                   message: response.data.message,
+    //                 });
+    //               } else {
+    //                 setMessage({
+    //                   status: "warning",
+    //                   message: response.data.message,
+    //                 });
+    //               }
+    //             } catch (error) {
+    //               setMessage({
+    //                 status: "error",
+    //                 message: "Something went wrong. Please try again later.",
+    //               });
+    //             }
+    //           }}
+    //         >
+    //           {(props) => (
+    //             <Form>
+    //               <Field name="email">
+    //                 {({ field, form }) => (
+    //                   <FormControl
+    //                     isInvalid={form.errors.email && form.touched.email}
+    //                   >
+    //                     <FormLabel>Email</FormLabel>
+    //                     <Input type="email" {...field} placeholder="Email" />
+    //                     <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+    //                   </FormControl>
+    //                 )}
+    //               </Field>
+    //               <Field name="password">
+    //                 {({ field, form }) => (
+    //                   <FormControl
+    //                     isInvalid={
+    //                       form.errors.password && form.touched.password
+    //                     }
+    //                   >
+    //                     <FormLabel>Password</FormLabel>
+    //                     <Input
+    //                       type="password"
+    //                       {...field}
+    //                       placeholder="Password"
+    //                     />
+    //                     <FormErrorMessage>
+    //                       {form.errors.password}
+    //                     </FormErrorMessage>
+    //                   </FormControl>
+    //                 )}
+    //               </Field>
+    //               <Button
+    //                 mt={4}
+    //                 colorScheme="teal"
+    //                 isLoading={props.isSubmitting}
+    //                 type="submit"
+    //               >
+    //                 Login
+    //               </Button>
+    //             </Form>
+    //           )}
+    //         </Formik>
+    //         {message && (
+    //           <Box py={4}>
+    //             <Alert status={message.status}>
+    //               <AlertIcon />
+    //               {message.message}
+    //             </Alert>
+    //           </Box>
+    //         )}
+    //         <Box mt={8}>
+    //           <UnorderedList>
+    //             <ListItem>
+    //               <Link href="/">Homepage</Link>
+    //             </ListItem>
+    //             <ListItem>
+    //               <Link href="/auth/register">Register</Link>
+    //             </ListItem>
+    //             <ListItem>
+    //               <Link href="/auth/forget">Forget Password</Link>
+    //             </ListItem>
+    //           </UnorderedList>
+    //         </Box>
+    //       </Box>
+    //     </Box>
+    //   </Box>
+    // </Container>
+    <LoginForm />
   );
 };
 
