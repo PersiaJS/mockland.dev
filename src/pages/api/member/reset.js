f3803e3b521ca52958f83134b0be057b24f52462
Maral Najafi <marawlnajafi@gmail.com>
2023-05-09 13:37:53 +0330

    fix(#37): reset method, replaced with PUT(backend)

--- src/pages/api/member/reset.js
+++ src/pages/api/member/reset.js
@@ -4,7 +4,7 @@ import methodMiddleware from "@/middlewares/methodMiddleware";
 import prisma from "../../../../lib/prisma";
 
 const handler = async (req, res) => {
-  await methodMiddleware(req, res, "POST");
+  await methodMiddleware(req, res, "PUT");
 
   const rules = {
     password: "required|min:6",
