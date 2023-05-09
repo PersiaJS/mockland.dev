e1fa621b3f10e1c77dd8c8d4364781eec56bb5ec
Maral Najafi <marawlnajafi@gmail.com>
2023-05-09 11:39:44 +0330

    feature(#37): integrate reset API, handle submit

--- src/components/ResetPasswordForm/ResetPasswordForm.jsx
+++ src/components/ResetPasswordForm/ResetPasswordForm.jsx
@@ -15,7 +15,6 @@ import {
   Alert,
   AlertIcon,
 } from "@chakra-ui/react";
-import { GoogleIcon } from "@/utils/createIcon";
 import { Formik, Field } from "formik";
 import * as Yup from "yup";
 import fetchHandler from "../../utils/fetchHandler";
@@ -30,6 +29,7 @@ const ResetPasswordSchema = Yup.object().shape({
 
 const ResetPasswordForm = () => {
   const [message, setMessage] = useState(null);
+  const [isPending, setIsPending] = useState(false);
 
   return (
     <Container
@@ -55,9 +55,38 @@ const ResetPasswordForm = () => {
           passwordConfirm: "",
         }}
         validationSchema={ResetPasswordSchema}
+        onSubmit={async (values, { resetForm }) => {
+          setIsPending(true);
+          try {
+            const response = await fetchHandler.post(
+              "/api/member/reset",
+              values
+            );
+            if (response.data.status) {
+              setMessage({
+                status: "success",
+                message: response.data.message,
+              });
+              setIsPending(false);
+              resetForm();
+            } else {
+              setIsPending(false);
+              setMessage({
+                status: "warning",
+                message: response.data.message,
+              });
+            }
+          } catch (error) {
+            setIsPending(false);
+            setMessage({
+              status: "error",
+              message: "Something went wrong. Please try again later.",
+            });
+          }
+        }}
       >
-        {({ errors, touched }) => (
-          <form>
+        {({ handleSubmit, errors, touched }) => (
+          <form onSubmit={handleSubmit}>
             <VStack spacing={5}>
               <FormControl isInvalid={errors.password && touched.password}>
                 <FormLabel htmlFor="password" color={"blackAlpha.700"}>
@@ -89,7 +118,12 @@ const ResetPasswordForm = () => {
                 <FormErrorMessage>{errors.passwordConfirm}</FormErrorMessage>
               </FormControl>
               <VStack align="stretch" width={"100%"} spacing={3}>
-                <Button variant={"solid"} colorScheme="green" type="submit">
+                <Button
+                  variant={"solid"}
+                  colorScheme="green"
+                  type="submit"
+                  isLoading={isPending}
+                >
                   Reset
                 </Button>
               </VStack>
