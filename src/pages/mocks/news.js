2fef6230fe26ea6f9c50179891ba3c5e418c71b3
Ehsan Gazar <me@ehsangazar.com>
2023-05-23 09:29:06 +1000

    news docs is ready

--- src/pages/mocks/news.js
+++ src/pages/mocks/news.js
@@ -1,44 +1,84 @@
-import Layout from "@/components/Layout/Layout";
-import {
-  Box,
-  Breadcrumb,
-  BreadcrumbItem,
-  Container,
-  Heading,
-} from "@chakra-ui/react";
-import Link from "next/link";
+import { Box, Text, Container, Flex } from "@chakra-ui/react";
+import LayoutWithSideBar from "@/components/LayoutWithSideBar/LayoutWithSideBar";
 
-const MockNewsIndex = () => {
+function NewsDocs() {
   return (
-    <Layout isActiveSideBar={true}>
-    <Container>
-      <Box display={"grid"} alignItems={"center"} height={"100vh"}>
-        <Box>
-          <Breadcrumb>
-            <BreadcrumbItem>
-              <Link href="/"> HomePage </Link>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-              <Link href="/mocks"> Mocks </Link>
-            </BreadcrumbItem>
-            <BreadcrumbItem isCurrentPage>
-              <Link href="/mocks/news"> News </Link>
-            </BreadcrumbItem>
-          </Breadcrumb>
-          <Heading as={"h1"}>News</Heading>
-          <Box my={8}>
-            <p>GET: https://mockland.dev/api/news/list</p>
-            <p>GET: https://mockland.dev/api/news/get-id/:id</p>
-            <p>GET: https://mockland.dev/api/news/get-slug/:slug</p>
-            <p>PUT: https://mockland.dev/api/news/update/:id</p>
-            <p>DELETE: https://mockland.dev/api/news/delete/:id</p>
-            <p>POST: https://mockland.dev/api/news/create</p>
-          </Box>
-        </Box>
-      </Box>
-      </Container>
-      </Layout>
+    <>
+      <LayoutWithSideBar>
+        <main>
+          <Flex gap={12}>
+            <Container maxW="4xl">
+              <Box display={"grid"} alignItems={"center"}>
+                <Text fontSize="5xl">NewsDocs Page</Text>
+                <Text mt={5}>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                  ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                  nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </Text>
+                <p>GET: https://mockland.dev/api/news/list</p>
+                <p>GET: https://mockland.dev/api/news/get-id/:id</p>
+                <p>GET: https://mockland.dev/api/news/get-slug/:slug</p>
+                <p>PUT: https://mockland.dev/api/news/update/:id</p>
+                <p>DELETE: https://mockland.dev/api/news/delete/:id</p>
+                <p>POST: https://mockland.dev/api/news/create</p>
+                <br />
+                <Box bg="black" w="100%" p={4} color="white">
+                  <Box
+                    as="button"
+                    borderRadius="md"
+                    bg="blue.600"
+                    color="white"
+                    px={4}
+                    h={8}
+                  >
+                    copy
+                  </Box>
+                  <Text mt={5}> #first step</Text>
+                  <Text mt={6} color={"blue.600"}>
+                    npm i @chakra-ui/react @emotion/react @emotion/styled
+                    framer-motion
+                  </Text>
+                </Box>
+                <Text mt={5}>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                  ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                  nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </Text>
+                <br />
+                <Text fontSize="5xl">NewsDocs Page</Text>
+                <Text mt={5} pb={10}>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                  ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                  nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </Text>
+              </Box>
+            </Container>
+          </Flex>
+        </main>
+      </LayoutWithSideBar>
+    </>
   );
-};
+}
 
-export default MockNewsIndex;
+export default NewsDocs;
