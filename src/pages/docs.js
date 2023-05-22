91a50815e1fa8d336625fdf3b3df167982f476c8
Hossein-K-R <116842837+Hossein-K-R@users.noreply.github.com>
2023-05-22 16:27:20 +0200

    Create Docs
    Mock API Documentation Center Section

---
+++ src/pages/docs.js
@@ -0,0 +1,78 @@
+import { Box, Text, Container, Flex } from "@chakra-ui/react";
+import LayoutWithSideBar from "@/components/LayoutWithSideBar/LayoutWithSideBar";
+
+function Docs() {
+  return (
+    <>
+      <LayoutWithSideBar>
+        <main>
+          <Flex gap={12} direction={{ base: "column", md: "row" }}>
+            <Container>
+              <Box display={"grid"} alignItems={"center"}>
+                <Text fontSize="5xl">Docs Page</Text>
+                <Text mt={5}>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                  ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                  nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </Text>
+                <br />
+                <Box bg="black" w="100%" p={4} color="white">
+                  <Box
+                    as="button"
+                    borderRadius="md"
+                    bg="blue.600"
+                    color="white"
+                    px={4}
+                    h={8}
+                  >
+                    copy
+                  </Box>
+                  <Text mt={5}> #first step</Text>
+                  <Text mt={6} color={'blue.600'}>
+                    npm i @chakra-ui/react @emotion/react @emotion/styled
+                    framer-motion
+                  </Text>
+                </Box>
+                <Text mt={5}>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                  ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                  nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </Text>
+                <br />
+                <Text fontSize="5xl">Docs Page</Text>
+                <Text mt={5} pb={10}>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                  ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                  nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                  sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </Text>
+              </Box>
+            </Container>
+          </Flex>
+        </main>
+      </LayoutWithSideBar>
+    </>
+  );
+}
+
+export default Docs;
