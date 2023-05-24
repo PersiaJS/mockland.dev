c1d49710b24bacb1a7c4d93ea85e8bb0cd90b462
Maral Najafi <marawlnajafi@gmail.com>
2023-05-24 19:50:26 +0330

    fix: mocks grid

--- src/pages/mocks/index.js
+++ src/pages/mocks/index.js
@@ -16,48 +16,39 @@ const MocksIndex = () => {
   return (
     <LayoutWithSideBar>
       <Flex flexDir={"column"} px={6}>
-            <Breadcrumb my={2}>
-              <BreadcrumbItem>
-                <Link href="/"> HomePage </Link>
-              </BreadcrumbItem>
-              <BreadcrumbItem isCurrentPage>
-                <Link href="/mocks"> Mocks </Link>
-              </BreadcrumbItem>
-            </Breadcrumb>
-            <Heading as={"h1"}>Mocks</Heading>
-            <Grid
-              my={8}
-              gridGap={4}
-              templateRows={{
-                base: "repeat(1, 1fr)",
-                md: "repeat(2, 1fr)",
-                lg: "repeat(3, 1fr)",
-              }}
-              templateColumns={{
-                base: "repeat(1, 1fr)",
-                md: "repeat(2, 1fr)",
-                lg: "repeat(3, 1fr)",
-              }}
-            >
-              <GridItem>
-                <MocksCards title="News" link="news" />
-              </GridItem>
-              <GridItem>
-                <MocksCards title="Product" link="product" />
-              </GridItem>
-              <GridItem>
-                <MocksCards title="Post" link="post" />
-              </GridItem>
-              <GridItem>
-                <MocksCards title="Category" link="category" />
-              </GridItem>
-              <GridItem>
-                <MocksCards title="Auth" link="auth" />
-              </GridItem>
-              <GridItem>
-                <MocksCards title="Cart" link="cart" />
-              </GridItem>
-            </Grid>
+        <Breadcrumb my={2}>
+          <BreadcrumbItem>
+            <Link href="/"> HomePage </Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem isCurrentPage>
+            <Link href="/mocks"> Mocks </Link>
+          </BreadcrumbItem>
+        </Breadcrumb>
+        <Heading as={"h1"}>Mocks</Heading>
+        <Grid
+          my={8}
+          gridGap={4}
+          templateColumns={"repeat(auto-fill, minmax(300px, 1fr))"}
+        >
+          <GridItem>
+            <MocksCards title="News" link="news" />
+          </GridItem>
+          <GridItem>
+            <MocksCards title="Product" link="product" />
+          </GridItem>
+          <GridItem>
+            <MocksCards title="Post" link="post" />
+          </GridItem>
+          <GridItem>
+            <MocksCards title="Category" link="category" />
+          </GridItem>
+          <GridItem>
+            <MocksCards title="Auth" link="auth" />
+          </GridItem>
+          <GridItem>
+            <MocksCards title="Cart" link="cart" />
+          </GridItem>
+        </Grid>
       </Flex>
     </LayoutWithSideBar>
   );
