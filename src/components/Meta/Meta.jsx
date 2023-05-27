32ba29db21e7011270dde9d942a3c17ae491f5f9
Hossein-K-R <116842837+Hossein-K-R@users.noreply.github.com>
2023-05-27 10:44:39 +0200

    Create Meta tags
    I have created the Meta component.I used the Meta tags only on the privacy policy page.We can use it on other pages.

---
+++ src/components/Meta/Meta.jsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+const Meta = ({ title, description, image }) => (
+  <Head>
+    {/* Standard metadata */}
+    <title>{title}</title>
+    <meta name="description" content={description} />
+
+    {/* Open Graph metadata */}
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content={image} />
+    <meta property="og:image:alt" content={title} />
+    <meta property="og:type" content="website" />
+    <meta property="og:locale" content="en_US" />
+
+    {/* Additional metadata */}
+    <meta name="twitter:card" content="Mockland" />
+  </Head>
+);
+
+export default Meta;
