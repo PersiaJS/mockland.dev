f6eeb79effbd8a0a9d50243b05dc80babc80b917
Ali Ahmadi <mrkiller1383@gmail.com>
2023-06-04 14:08:09 +0330

    Wrote test for http method  for the /news/create route in create.test.js

--- tests/integrations/news/create.test.js
+++ tests/integrations/news/create.test.js
@@ -2,17 +2,40 @@ const { default: axios } = require("axios");
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://mockland.dev";
 const endpoint = "/api/news/create";
 const url = baseUrl + endpoint;
+const token = process.env.MEMBER_TEST_TOKEN;
 
 describe(endpoint + "/list", () => {
-  xit("Should respond with code 422 if validation fails", async () => {
-    const res = await axios.post(url, {
-      title: "test",
-      description: "test",
-      content: "test",
-      image: "test",
-      slug: "test",
-      author: "test",
-    });
-    expect(res.status).toBe(422);
+  it("Should respond with code 405 if method is not POST", async () => {
+    try {
+      await axios.get(url, {
+        title: "test",
+        description: "test",
+        content: "test",
+        image: "test",
+        slug: "test",
+        author: "test",
+      });
+    } catch (err) {
+      expect(err.response.status).toBe(405);
+    }
+  });
+
+  xit("Should respond with code 405 if method is not POST", async () => {
+    try {
+      await axios.post(
+        url,
+        {
+          title: "test",
+          description: "test",
+          content: "test",
+          image: "test",
+          slug: "test",
+          author: "test",
+        },
+        { headers: { token } }
+      );
+    } catch (err) {
+      expect(err.response.status).toBe(422);
+    }
   });
 });
